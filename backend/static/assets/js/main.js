843e754881a97e157fa8834ed6628b58bbdc504f
manasam27 <75308392+manasam27@users.noreply.github.com>
2022-08-21 18:43:25 +0530

    Update main.js

--- backend/static/assets/js/main.js
+++ backend/static/assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Medilab - v4.7.0
-* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
